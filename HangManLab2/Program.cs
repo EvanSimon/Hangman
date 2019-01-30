20b18fb3232b4992bb9989ac0eb596d03a901b61
EvanSimon <evansimon07@gmail.com>
2019-01-29 22:16:58 -0500

    Anyone ever relize how dark of a game this is

---
+++ HangManLab2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangManStarterKit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Player p2 = new RandomPlayer();//to call the diffrent players add there name after new
+            HangmanGame hg = new HangmanGame(p2);
+            Console.WriteLine();
+        }
+    }
+}
